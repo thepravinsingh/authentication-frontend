6bd869abba52c29e290bbb06f7570819b557bb19
Praveen Singh <singhpravu@gmail.com>
2025-01-31 00:42:55 +0530

    Refactor Login component layout and styling; remove unused state variable

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,8 @@
 "use client";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Login() {
-  const [storeData, setStoreData] = useState({});
   const [data, setFormData] = useState({
     username: "",
     email: "",
@@ -42,60 +41,61 @@ export default function Login() {
     fetchData();
   };
 
-  console.log(storeData);
-
   return (
     <div className="flex flex-col  justify-center items-center h-screen">
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          name="username"
-          value={data.username}
-          onChange={handleChange}
-          className="border-2 rounded-md text-[0.8rem] "
-        />
-      </div>
+      <div className=" flex flex-col justify-between gap-2 p-4 border rounded-md  shadow-md">
+        <h4 className="font-semibold text-[1.2rem] self-center">Signup</h4>
+        <div className="flex justify-between w-full gap-1 text-[0.8rem] font-medium">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={data.username}
+            onChange={handleChange}
+            className="border-2 rounded-md text-[0.8rem] "
+          />
+        </div>
 
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          value={data.email}
-          onChange={handleChange}
-          className="border-2 rounded-md text-[0.8rem] "
-        />
-      </div>
+        <div className="flex justify-between w-full gap-1 text-[0.8rem] font-medium">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={data.email}
+            onChange={handleChange}
+            className="border-2 rounded-md text-[0.8rem] "
+          />
+        </div>
 
-      <div>
-        <label htmlFor="phone">Phone</label>
-        <input
-          type="number"
-          name="phone"
-          value={data.phone}
-          onChange={handleChange}
-          className="border-2 rounded-md text-[0.8rem] "
-        />
-      </div>
+        <div className="flex justify-between w-full gap-1 text-[0.8rem] font-medium">
+          <label htmlFor="phone">Phone:</label>
+          <input
+            type="number"
+            name="phone"
+            value={data.phone}
+            onChange={handleChange}
+            className="border-2 rounded-md text-[0.8rem] "
+          />
+        </div>
 
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="text"
-          name="password"
-          value={data.password}
-          onChange={handleChange}
-          className="border-2 rounded-md text-[0.8rem] "
-        />
-      </div>
+        <div className="flex justify-between w-full gap-1 text-[0.8rem] font-medium">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="text"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+            className="border-2 rounded-md text-[0.8rem] "
+          />
+        </div>
 
-      <button
-        onClick={handleSubmit}
-        className="border-2 bg-blue-500 hover:bg-blue-300 text-white px-4 py-2 rounded-lg"
-      >
-        Submit
-      </button>
+        <button
+          onClick={handleSubmit}
+          className="border-2 bg-blue-500 hover:bg-blue-300 text-white text-[0.8rem] px-4 py-1 rounded-lg"
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
